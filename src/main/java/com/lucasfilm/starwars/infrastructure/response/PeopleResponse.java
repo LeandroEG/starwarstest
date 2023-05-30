b1d967001cf6afc7ccf16865670baf52ed5826e7
LeandroEG <56354398+LeandroEG@users.noreply.github.com>
2023-05-30 19:18:29 +0200

    Incluimos atributo next para controlar la paginación

--- src/main/java/com/lucasfilm/starwars/infrastructure/response/PeopleResponse.java
+++ src/main/java/com/lucasfilm/starwars/infrastructure/response/PeopleResponse.java
@@ -3,7 +3,18 @@ package com.lucasfilm.starwars.infrastructure.response;
 import java.util.List;
 
 public class PeopleResponse {
+
+    private String next;
+
     private List<PersonResponse> results;
+
+    public String getNext() {
+        return next;
+    }
+
+    public void setNext(String next) {
+        this.next = next;
+    }
 
     public List<PersonResponse> getResults() {
         return results;
